7297bfe7e83b6477bf3a9ed8c4b5d38921584e7e
Christer Jonassen <christer@csdnserver.com>
2012-11-08 13:49:20 +0100

    Incremental refresh update
    A new test for implementing incremental refreshes

--- watchdoglib.sh
+++ watchdoglib.sh
@@ -152,6 +152,7 @@ pinghosts()
 	while read -r HOSTENTRY
 	do
 		Y=$(( Y + 1 ))
+		if [ $FIRSTDRAW == YES ]; then
 		HOSTDESC=`echo $HOSTENTRY | awk -F":" '{print $1}' $2`
 		HOSTLOC=`echo $HOSTENTRY | awk -F":" '{print $2}' $2`
 		HOSTIP=`echo $HOSTENTRY | awk -F":" '{print $3}' $2`
@@ -163,6 +164,9 @@ pinghosts()
 		upforward 35
 		echo -e " $HOSTIP"
 		upforward 53
+		FIRSTDRAW=NO
+		fi
+		if [ $FIRSTDRAW == NO ]; then; fi
 		echo -e " "$DEF"[   "$LIGHTYELLOW"Ping in progress..  "$DEF"]"$GRAY""
 		HOSTLAT=`ping -q -c 3 -n -i 0.2 -W1 $HOSTIP | tail -1| awk '{print $4}' | cut -d '/' -f 2`
 		HOSTLAT="$HOSTLAT ms"
