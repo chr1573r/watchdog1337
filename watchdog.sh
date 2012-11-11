a63f6d014d12637a8519056a7f93164f9b524467
Christer Jonassen <christer@csdnserver.com>
2012-11-11 02:06:41 +0100

    Adjusting trap
    Get a proper exit message

--- watchdog.sh
+++ watchdog.sh
@@ -7,7 +7,7 @@ APPVERSION=0.1
 REDRAW=YES
 
 # Set trap for catching Ctrl-C and kills
-trap "{ Watchdog terminated at `date` ; reset; exit; }" SIGINT SIGTERM
+trap "{ reset; echo Watchdog1337 $APPVERSION terminated at `date`; exit; }" SIGINT SIGTERM
 
 clear
 echo Loading library..
