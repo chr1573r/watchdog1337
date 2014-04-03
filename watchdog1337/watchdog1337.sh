bb88773e0d30705f6036b549b252de8998ef2163
Christer Jonassen <christer@csdnserver.com>
2014-04-03 02:37:26 +0200

    V 1.2
    Features added:
    + New optional slim interface for higher host density.
    + Label of column 2 (location) can now be changed via settings.cfg.
    Useful in scenarios where "role" would be more useful than location etc.
    
    Improvements:
    + Ping "algorithm" now uses the same settings for each ping command
    execution
    + Backticks "`" replaced by "$()"

--- watchdog1337/watchdog1337.sh
+++ watchdog1337/watchdog1337.sh
@@ -7,12 +7,12 @@
 # Check README for instructions
 
 # Variables
-APPVERSION="1.1"
+APPVERSION="1.2"
 REDRAW=YES
-DOMAIN=`hostname -d` # Reads the domain part of the hostname, e.g. network.lan
-if [ -z "$DOMAIN" ]; then DOMAIN=hosts; fi # If domain part of hostname is blank, set text to "network"
-PREVIOUSCOLUMNS=`tput cols`
-PREVIOUSLINES=`tput lines`
+DOMAIN=$(hostname -d) # Reads the domain part of the hostname, e.g. network.lan
+if [ -z "$DOMAIN" ]; then DOMAIN=hosts; fi # If domain part of hostname is blank, set text to "hosts"
+PREVIOUSCOLUMNS=$(tput cols)
+PREVIOUSLINES=$(tput lines)
 # Pretty colors for the terminal:
 DEF="\x1b[0m"
 WHITE="\e[0;37m"
@@ -56,7 +56,7 @@ gfx () # Used to display repeating "graphics" where needed
 			echo -e ""$LIGHTRED"   | |/ |/ / ___ |/ / / /___/ __  / /_/ / /_/ / /_/ /  "$LIGHTYELLOW"  / /___/ /__/ / / /"$DEF""
 			echo -e ""$LIGHTRED"   |__/|__/_/  |_/_/  \____/_/ /_/_____/\____/\____/   "$LIGHTYELLOW" /_//____/____/ /_/"$DEF""
 			echo
-			echo -e "          "$RED"WATCHDOG "$YELLOW"1337 "$GRAY"Version $APPVERSION - "$RED"Cj Designs"$GRAY"/"$YELLOW"CSDNSERVER.COM"$GRAY" - 2012"
+			echo -e "          "$RED"WATCHDOG "$YELLOW"1337 "$GRAY"Version $APPVERSION - "$RED"Cj Designs"$GRAY"/"$YELLOW"CSDNSERVER.COM"$GRAY" - 2014"
 			sleep 2
 			clear
 			
@@ -68,19 +68,29 @@ gfx () # Used to display repeating "graphics" where needed
 
 		header)
 			clear
-			echo -e ""$RED"  _       _____  ______________  ______  ____  ______"$YELLOW"   __________________"$DEF""
-			echo -e ""$RED" | |     / /   |/_  __/ ____/ / / / __ \/ __ \/ ____/ "$YELLOW" <  /__  /__  /__  /"$DEF""
-			echo -e ""$RED" | | /| / / /| | / / / /   / /_/ / / / / / / / / __  "$YELLOW"  / / /_ < /_ <  / /"$DEF""
-			echo -e ""$RED" | |/ |/ / ___ |/ / / /___/ __  / /_/ / /_/ / /_/ /  "$YELLOW" / /___/ /__/ / / /"$DEF""
-			echo -e ""$RED" |__/|__/_/  |_/_/  \____/_/ /_/_____/\____/\____/   "$YELLOW"/_//____/____/ /_/"$DEF""
-			echo
+			if [[ "$SLIM" != "1" ]]; then
+				echo -e ""$RED"  _       _____  ______________  ______  ____  ______"$YELLOW"   __________________"$DEF""
+				echo -e ""$RED" | |     / /   |/_  __/ ____/ / / / __ \/ __ \/ ____/ "$YELLOW" <  /__  /__  /__  /"$DEF""
+				echo -e ""$RED" | | /| / / /| | / / / /   / /_/ / / / / / / / / __  "$YELLOW"  / / /_ < /_ <  / /"$DEF""
+				echo -e ""$RED" | |/ |/ / ___ |/ / / /___/ __  / /_/ / /_/ / /_/ /  "$YELLOW" / /___/ /__/ / / /"$DEF""
+				echo -e ""$RED" |__/|__/_/  |_/_/  \____/_/ /_/_____/\____/\____/   "$YELLOW"/_//____/____/ /_/"$DEF""
+				echo
+			fi
 			;;
 		subheader)
-			timeupdate
-			tput cup 6 0
-			echo -e "$RED""///"$GRAY" Watching "$YELLOW"$DOMAIN"$GRAY" from "$YELLOW""`hostname -s`" "$RED"/// "$GRAY"Load:`uptime | awk -F'load average:' '{ print $2 }'`"$RED" ///"$GRAY" $HM"$DEF""
-			echo
-			echo
+			if [[ "$SLIM" != "1" ]]; then
+				timeupdate
+				tput cup 6 0
+				echo -e "$RED""///"$GRAY" Watching "$YELLOW"$DOMAIN"$GRAY" from "$YELLOW""$(hostname -s)" "$RED"/// "$GRAY"Load:$(uptime | awk -F'load average:' '{ print $2 }')"$RED" ///"$GRAY" $HM"$DEF""
+				echo
+				echo
+			else
+				timeupdate
+				tput cup 0 0 
+				echo -e ""$RED"/// Watchdog "$YELLOW"1337 "$RED"///"$GRAY" Watching "$YELLOW"$DOMAIN"$GRAY" from "$YELLOW""$(hostname -s)""
+				#tput cup 1 0
+				#echo -e "$RED""///"$GRAY" Watching "$YELLOW"$DOMAIN"$GRAY" from "$YELLOW""$(hostname -s)" "$RED"/// "$GRAY"Load:$(uptime | awk -F'load average:' '{ print $2 }')"$RED" ///"$GRAY" $HM"$DEF""
+			fi
 			;;
 	esac
 }
@@ -96,22 +106,66 @@ timeupdate() # Sets current time into different variables. Used for timestamping
 	MINUTE=$(date +"%M")
 	SEC=$(date +"%S")
 }
+
+printheader() # Draw the header (column labels)
+{
+	if [[ "$SLIM" != "1" ]]; then
+		HEADYPOS=9
+	else
+		HEADYPOS=2
+	fi
+	tput cup $HEADYPOS 0
+	echo -e ""$DEF""$LIGHTYELLOW"NAME"
+	tput cup $HEADYPOS 15
+	echo -e "$COL2"
+	tput cup $HEADYPOS 36
+	echo -e "ADDRESS"
+	tput cup $HEADYPOS 54
+	echo -e "AVG.LATENCY"
+	tput cup $HEADYPOS 73
+	echo -e "STATUS"$DEF""
+	gfx line
+}
+
+
+upforward() # Move up one line in terminal and jump to horisontal posistion specified
+{
+	#
+	tput cup $Y $1
+}
+
+termreset()
+{
+		echo Terminal size changed, resetting...
+		PREVIOUSCOLUMNS=$(tput cols)
+		PREVIOUSLINES=$(tput lines)
+		REDRAW=YES
+		reset
+		gfx header
+}
+
+
 pinghosts() # Parses hosts.lst into variables, pings host, displays output based on ping result
 {
-	Y=10
+	if [[ "$SLIM" != "1" ]]; then
+		Y=10
+	else
+		Y=3
+	fi
 	HOSTS=0
 	HOSTSOK=0
 	HOSTSDOWN=0
+	if [ "$REDRAW" == "YES" ] ; then printheader; fi
+		#if [ "$REDRAW" == "YES" ] ; then echo -e ""$DEF""$LIGHTYELLOW"NAME           LOCATION             ADDRESS           AVG.LATENCY        STATUS"$DEF""; gfx line; fi
 
-	if [ "$REDRAW" == "YES" ] ; then echo -e ""$DEF""$LIGHTYELLOW"NAME           LOCATION             ADDRESS           AVG.LATENCY        STATUS"$DEF""; gfx line; fi
 	while read -r HOSTENTRY
 		do
 			Y=$(( Y + 1 ))
 			HOSTS=$(( HOSTS + 1))
 			
-			HOSTDESC=`echo $HOSTENTRY | awk -F":" '{print $1}' $2`
-			HOSTLOC=`echo $HOSTENTRY | awk -F":" '{print $2}' $2`
-			HOSTIP=`echo $HOSTENTRY | awk -F":" '{print $3}' $2`
+			HOSTDESC=$(echo $HOSTENTRY | awk -F":" '{print $1}' $2)
+			HOSTLOC=$(echo $HOSTENTRY | awk -F":" '{print $2}' $2)
+			HOSTIP=$(echo $HOSTENTRY | awk -F":" '{print $3}' $2)
 			#echo YOLO $HOSTENTRY BRO $HOSTDESC $HOSTLOC $HOSTIP $Y
 			if [ "$REDRAW" == "YES" ] ; then
 				tput el
@@ -126,10 +180,10 @@ pinghosts() # Parses hosts.lst into variables, pings host, displays output based
 			
 			upforward 53	
 			echo -e " "$WHITE"[   "$LIGHTYELLOW"Ping in progress..  "$WHITE"]"$DEF""
-			
-			ping -q -c 2 -n -i 0.2 -W1 $HOSTIP &> /dev/null
+			# Currently, we execute ping up to two times per host. This is due to parcing replacing the exit code from ping. Hopefully a better solution will be found later.
+			ping -q -c $PING_COUNT -n -i $PING_INTERVAL -W $PING_TIMEOUT $HOSTIP &> /dev/null	# Ping first to get exit code
 				if [ $? == 0 ]; then
-					HOSTLAT=`ping -q -c $PING_COUNT -n -i $PING_INTERVAL -W $PING_TIMEOUT $HOSTIP | tail -1| awk '{print $4}' | cut -d '/' -f 2`
+					HOSTLAT=$(ping -q -c $PING_COUNT -n -i $PING_INTERVAL -W $PING_TIMEOUT $HOSTIP | tail -1 | awk '{print $4}' | cut -d '/' -f 2) &> /dev/null # ping again to get avg latency parced
 					HOSTLAT="$HOSTLAT ms"
 					upforward 53
 					tput el
@@ -162,21 +216,6 @@ pinghosts() # Parses hosts.lst into variables, pings host, displays output based
 		if [ "$HOSTSOK" == "$HOSTS" ] ; then REDRAW=NO; else REDRAW=YES; fi # If any hosts failed, we want to redraw next round
 }
 
-upforward() # Move up one line in terminal and jump to horisontal posistion specified
-{
-	#
-	tput cup $Y $1
-}
-
-termreset()
-{
-		echo Terminal size changed, resetting...
-		PREVIOUSCOLUMNS=`tput cols`
-		PREVIOUSLINES=`tput lines`
-		REDRAW=YES
-		reset
-		gfx header
-}
 
 summarynext() #Displays a status summary and statistics and waits the number of seconds determined by REFRESHRATE
 {
@@ -203,8 +242,8 @@ summarynext() #Displays a status summary and statistics and waits the number of 
 		tput rc
 		tput el
 	done
-	CURRENTCOLUMNS=`tput cols`
-	CURRENTLINES=`tput lines`
+	CURRENTCOLUMNS=$(tput cols)
+	CURRENTLINES=$(tput lines)
 	if [ "$PREVIOUSCOLUMNS" != "$CURRENTCOLUMNS" ]; then
 		termreset
 
@@ -220,7 +259,7 @@ summarynext() #Displays a status summary and statistics and waits the number of 
 
 # The actual runscript:
 
-trap "{ reset; clear;echo Watchdog1337 $APPVERSION terminated at `date`; exit; }" SIGINT SIGTERM EXIT # Set trap for catching Ctrl-C and kills, so we can reset terminal upon exit
+trap "{ reset; clear;echo Watchdog1337 $APPVERSION terminated at $(date); exit; }" SIGINT SIGTERM EXIT # Set trap for catching Ctrl-C and kills, so we can reset terminal upon exit
 
 clear
 gfx splash # Display splash screen with logo
@@ -230,6 +269,7 @@ if [ -f settings.cfg ] ; then source settings.cfg; fi
 if [ -n "$1" ]; then REFRESHRATE=$1; fi # Sets $1 as refreshrate, if it is not null. Overrides value set in settings.cfg
 
 echo Validating configuration... # Check if important variables contain anything. If they are empty, default values will be set.
+if [ -z "$COL2" ]; then echo -e ""$YELLOW"WATCHDOG Warning: "$GRAY"COL2 not set, changing COL2 to "LOCATION"."$DEF""; COL2=LOCATION; sleep 1; fi
 if [ -z "$REFRESHRATE" ]; then echo -e ""$YELLOW"WATCHDOG Warning: "$GRAY"REFRESHRATE not set, changing REFRESHRATE to 5 seconds."$DEF""; REFRESHRATE=5; sleep 1; fi
 if [ -z "$CUSTOMCMDENABLE" ]; then echo -e ""$YELLOW"WATCHDOG Warning: "$GRAY"CUSTOMCMDENABLE not set, changing CUSTOMCMDENABLE to 0."$DEF""; CUSTOMCMDENABLE=0; sleep 1; fi
 if [ -z "$CUSTOMCMD" ]; then echo -e ""$YELLOW"WATCHDOG Warning: "$GRAY"CUSTOMCMD not set, changing CUSTOMCMDENABLE to 0."$DEF""; CUSTOMCMDENABLE=0; sleep 1; fi
